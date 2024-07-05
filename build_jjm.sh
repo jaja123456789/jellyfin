238f4ba715829de03d6401f5f28826387026d688
DESKTOP-JMR591K\Administrator <jaon997@gmail.com>
2024-07-05 12:14:52 +0200

    Build file edit

--- build_jjm.sh
+++ build_jjm.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-VERSION=v10.8.10
+VERSION=v10.9.7
 STAGE=${1:-0}
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 if [[ $STAGE -le 0 ]]; then
     # debian with jellyfin-ffmpeg and fonts
-    docker build -t jjm2473/jellyfin-runtime:v1 -f Dockerfile.runtime deployment || exit 1
-    docker push jjm2473/jellyfin-runtime:v1
+    docker build -t jaja123456/jellyfin-runtime:v1 -f Dockerfile.runtime deployment || exit 1
+    #docker push jjm2473/jellyfin-runtime:v1
 fi
 
 if [[ $STAGE -le 1 ]]; then
     # build this jellyfin server
-    docker build -t jjm2473/jellyfin-server-build:$VERSION -f deployment/Dockerfile.docker.arm64 . || exit 1
-    docker build -t jjm2473/jellyfin-server:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.server deployment || exit 1
-    docker push jjm2473/jellyfin-server:$VERSION
+    docker build -t jaja123456/jellyfin-server-build:$VERSION -f deployment/Dockerfile.docker.arm64 . || exit 1
+    docker build -t jaja123456/jellyfin-server:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.server deployment || exit 1
+    #docker push jjm2473/jellyfin-server:$VERSION
 fi
 
 if [[ $STAGE -le 3 ]]; then
@@ -27,10 +27,10 @@ if [[ $STAGE -le 3 ]]; then
 
     [[ -f common/jellyfin-web/index.html ]] || { echo "common/jellyfin-web/index.html not exists">&2 ; exit 1; }
     # create common image
-    docker build -t jjm2473/jellyfin-base:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.base common || exit 1
+    docker build -t jaja123456/jellyfin-base:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.base common || exit 1
     ## push local repo, ignore error
-    # docker tag jjm2473/jellyfin-base:$VERSION localhost:5000/jjm2473/jellyfin-base:$VERSION
-    # docker push localhost:5000/jjm2473/jellyfin-base:$VERSION 2>/dev/null
+    # docker tag jaja123456/jellyfin-base:$VERSION localhost:5000/jaja123456/jellyfin-base:$VERSION
+    # docker push localhost:5000/jaja123456/jellyfin-base:$VERSION 2>/dev/null
 
     docker push jjm2473/jellyfin-base:$VERSION
 fi
@@ -44,32 +44,32 @@ if [[ $STAGE -le 4 ]]; then
     # build rockchip image
     [ -f rockchip/Dockerfile.sample ] || ( mkdir rockchip && curl -L https://github.com/jjm2473/ffmpeg-rk/releases/download/v1.0/jellyfin-rkmpp-ffmpeg_5.1-2.git.tar | tar -x -C rockchip ) || exit 1
 
-    docker build --pull -t jjm2473/jellyfin-mpp:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.rk rockchip || exit 1
+    docker build --build-arg VERSION=$VERSION -f Dockerfile.rk rockchip || exit 1
 fi
 
 if [[ $STAGE = 5 ]]; then
-    docker push jjm2473/jellyfin-rtk:4.9-$VERSION || exit 1
-    docker push jjm2473/jellyfin-mpp:$VERSION || exit 1
+    #docker push jjm2473/jellyfin-rtk:4.9-$VERSION || exit 1
+    #docker push jjm2473/jellyfin-mpp:$VERSION || exit 1
 fi
 
 if [[ $STAGE = 6 ]]; then
-    DATE=`date '+%Y%m%d%H'`
-    docker tag jjm2473/jellyfin-rtk:4.9-$VERSION jjm2473/jellyfin-rtk:4.9-latest
-    docker tag jjm2473/jellyfin-mpp:$VERSION jjm2473/jellyfin-mpp:latest
-    docker push jjm2473/jellyfin-rtk:4.9-latest || exit 1
-    docker push jjm2473/jellyfin-mpp:latest || exit 1
-    docker tag jjm2473/jellyfin-rtk:4.9-$VERSION jjm2473/jellyfin-rtk:4.9-$DATE
-    docker tag jjm2473/jellyfin-mpp:$VERSION jjm2473/jellyfin-mpp:$DATE
-    docker push jjm2473/jellyfin-rtk:4.9-$DATE
-    docker push jjm2473/jellyfin-mpp:$DATE
+    #DATE=`date '+%Y%m%d%H'`
+    #docker tag jjm2473/jellyfin-rtk:4.9-$VERSION jjm2473/jellyfin-rtk:4.9-latest
+    #docker tag jjm2473/jellyfin-mpp:$VERSION jjm2473/jellyfin-mpp:latest
+    #docker push jjm2473/jellyfin-rtk:4.9-latest || exit 1
+    #docker push jjm2473/jellyfin-mpp:latest || exit 1
+    #docker tag jjm2473/jellyfin-rtk:4.9-$VERSION jjm2473/jellyfin-rtk:4.9-$DATE
+    #docker tag jjm2473/jellyfin-mpp:$VERSION jjm2473/jellyfin-mpp:$DATE
+    #docker push jjm2473/jellyfin-rtk:4.9-$DATE
+    #docker push jjm2473/jellyfin-mpp:$DATE
 fi
 
 # push test
 if [[ $STAGE = 7 ]]; then
-    docker tag jjm2473/jellyfin-rtk:4.9-$VERSION jjm2473/jellyfin-rtk:4.9-$VERSION-test
-    docker tag jjm2473/jellyfin-mpp:$VERSION jjm2473/jellyfin-mpp:$VERSION-test
-    docker push jjm2473/jellyfin-rtk:4.9-$VERSION-test || exit 1
-    echo "pushed jjm2473/jellyfin-rtk:4.9-$VERSION-test"
-    docker push jjm2473/jellyfin-mpp:$VERSION-test || exit 1
-    echo "pushed jjm2473/jellyfin-mpp:$VERSION-test"
+    #docker tag jjm2473/jellyfin-rtk:4.9-$VERSION jjm2473/jellyfin-rtk:4.9-$VERSION-test
+    #docker tag jjm2473/jellyfin-mpp:$VERSION jjm2473/jellyfin-mpp:$VERSION-test
+    #docker push jjm2473/jellyfin-rtk:4.9-$VERSION-test || exit 1
+    #echo "pushed jjm2473/jellyfin-rtk:4.9-$VERSION-test"
+    #docker push jjm2473/jellyfin-mpp:$VERSION-test || exit 1
+    #echo "pushed jjm2473/jellyfin-mpp:$VERSION-test"
 fi

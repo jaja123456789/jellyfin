0c1871d8af72a0f25073ca68ef046ccb2c43fb58
jaon997 <jaon997@gmail.com>
2024-07-05 17:58:35 +0200

    build fixes

--- build_jjm.sh
+++ build_jjm.sh
@@ -2,7 +2,7 @@
 VERSION=v10.9.7
 STAGE=${1:-0}
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 if [[ $STAGE -le 0 ]]; then
     # debian with jellyfin-ffmpeg and fonts
@@ -18,41 +18,43 @@ if [[ $STAGE -le 1 ]]; then
 fi
 
 if [[ $STAGE -le 3 ]]; then
-    if [[ $STAGE -le 2 || ! -f common/jellyfin-web/index.html ]]; then
-        # copy jellyfin web
-        rm -rf common/jellyfin-web
-        mkdir common
-        cp -a /Volumes/data/src/jellyfin-web/dist common/jellyfin-web
-    fi
+    #if [[ $STAGE -le 2 || ! -f common/jellyfin-web/index.html ]]; then
+    #    # copy jellyfin web
+    #    rm -rf common/jellyfin-web
+    #    mkdir common
+    #    cp -a /Volumes/data/src/jellyfin-web/dist common/jellyfin-web
+    #fi
 
-    [[ -f common/jellyfin-web/index.html ]] || { echo "common/jellyfin-web/index.html not exists">&2 ; exit 1; }
+    #[[ -f common/jellyfin-web/index.html ]] || { echo "common/jellyfin-web/index.html not exists">&2 ; exit 1; }
     # create common image
     docker build -t jaja123456/jellyfin-base:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.base common || exit 1
     ## push local repo, ignore error
     # docker tag jaja123456/jellyfin-base:$VERSION localhost:5000/jaja123456/jellyfin-base:$VERSION
     # docker push localhost:5000/jaja123456/jellyfin-base:$VERSION 2>/dev/null
 
-    docker push jjm2473/jellyfin-base:$VERSION
+    #docker push jjm2473/jellyfin-base:$VERSION
 fi
 
 if [[ $STAGE -le 4 ]]; then
     # build rtd1296 image
-    [ -f realtek/Dockerfile.sample ] || ( mkdir realtek && curl -L https://github.com/jjm2473/rtd1296_prebuilt_target/releases/download/v1.0/jellyfin-rtk-ffmpeg_4.0.3.tar | tar -x -C realtek ) || exit 1
+    #[ -f realtek/Dockerfile.sample ] || ( mkdir -p realtek && curl -L https://github.com/jjm2473/rtd1296_prebuilt_target/releases/download/v1.0/jellyfin-rtk-ffmpeg_4.0.3.tar | tar -z -x -C realtek ) || exit 1
 
-    docker build --pull -t jjm2473/jellyfin-rtk:4.9-$VERSION --build-arg VERSION=$VERSION -f Dockerfile.rtk realtek || exit 1
+    #docker build --pull -t jjm2473/jellyfin-rtk:4.9-latest --build-arg VERSION=$VERSION -f Dockerfile.rtk realtek || exit 1
 
     # build rockchip image
-    [ -f rockchip/Dockerfile.sample ] || ( mkdir rockchip && curl -L https://github.com/jjm2473/ffmpeg-rk/releases/download/v1.0/jellyfin-rkmpp-ffmpeg_5.1-2.git.tar | tar -x -C rockchip ) || exit 1
+    [ -f rockchip/Dockerfile.sample ] || ( mkdir -p rockchip && curl -L https://github.com/jjm2473/ffmpeg-rk/releases/download/v1.0/jellyfin-rkmpp-ffmpeg_5.1-2.git.tar | tar -z -x -C rockchip ) || exit 1
 
-    docker build --build-arg VERSION=$VERSION -f Dockerfile.rk rockchip || exit 1
+    docker build --pull=false -t jaja123456/jellyfin-mpp:$VERSION --build-arg VERSION=$VERSION -f Dockerfile.rk rockchip || exit 1
 fi
 
 if [[ $STAGE = 5 ]]; then
+    echo "-"
     #docker push jjm2473/jellyfin-rtk:4.9-$VERSION || exit 1
     #docker push jjm2473/jellyfin-mpp:$VERSION || exit 1
 fi
 
 if [[ $STAGE = 6 ]]; then
+    echo "-"
     #DATE=`date '+%Y%m%d%H'`
     #docker tag jjm2473/jellyfin-rtk:4.9-$VERSION jjm2473/jellyfin-rtk:4.9-latest
     #docker tag jjm2473/jellyfin-mpp:$VERSION jjm2473/jellyfin-mpp:latest
@@ -66,6 +68,7 @@ fi
 
 # push test
 if [[ $STAGE = 7 ]]; then
+    echo "-"
     #docker tag jjm2473/jellyfin-rtk:4.9-$VERSION jjm2473/jellyfin-rtk:4.9-$VERSION-test
     #docker tag jjm2473/jellyfin-mpp:$VERSION jjm2473/jellyfin-mpp:$VERSION-test
     #docker push jjm2473/jellyfin-rtk:4.9-$VERSION-test || exit 1
